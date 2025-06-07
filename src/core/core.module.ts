199f47b2a1a97fa7390604e6dd335ee8acc62513
gmpm292 <gmpm292@gmail.com>
2025-06-07 17:06:18 -0400

    ok

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,12 +3,21 @@ import { SystemUtilsService } from './services/system-utils.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../modules/users/entities/user.entity';
 import { ScopedAccessService } from '../modules/scoped-access/services/scoped-access.service';
+import { AppMailerService } from './mailer/app-mailer.service';
+import { IAppMailer } from './mailer/app-mailer.interface';
 
 @Global()
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [ScopedAccessService, SystemUtilsService],
-  exports: [ScopedAccessService, SystemUtilsService],
+  providers: [
+    ScopedAccessService,
+    SystemUtilsService,
+    {
+      provide: IAppMailer,
+      useClass: AppMailerService,
+    },
+  ],
+  exports: [ScopedAccessService, SystemUtilsService, IAppMailer],
 })
 export class CoreModule implements OnModuleInit {
   constructor(private readonly systemUtils: SystemUtilsService) {}
